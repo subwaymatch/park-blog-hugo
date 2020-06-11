164bb02e2ddb8ecc231b49413e4541b765dbb8db
Ye Joo Park <subwaymatch@gmail.com>
2020-06-11 06:24:19 -0500

    Testing Jupyter simple HTML export

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 # Convert notebook files to HTML
-jupyter nbconvert content/**/*.ipynb --to markdown
+jupyter nbconvert content/**/*.ipynb --to html --template basic
 
 # Copy images for notebooks to static
 cp -r content/notebooks/images/* static/images/
 
 # Build hugo
-hugo
+# hugo
